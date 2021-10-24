813979fc4249618e2aebc0faa5151ec84df8f096
Shane <slholloway@mun.ca>
2021-10-23 22:33:32 -0230

    Made bullets go right, rotated bullets to correct orientation

--- Assets/[Scripts]/BulletController.cs
+++ Assets/[Scripts]/BulletController.cs
@@ -1,11 +1,20 @@
-﻿using System.Collections;
+﻿/*
+ * Full Name        : Shane Holloway
+ * Student ID       : 101273911
+ * Date Modified    : October 23, 2021
+ * File             : BulletController.cs
+ * Description      : Controls bullet movement, as well as cleanup on collision or reaching screen boundary
+ * Revision History : V 1.0 - Made bullets move left-to-right instead of bottom-to-top
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletController : MonoBehaviour, IApplyDamage
 {
-    public float verticalSpeed;
-    public float verticalBoundary;
+    public float horizontalSpeed;
+    public float horizontalBoundary;
     public BulletManager bulletManager;
     public int damage;
     
@@ -24,12 +33,12 @@ public class BulletController : MonoBehaviour, IApplyDamage
 
     private void _Move()
     {
-        transform.position += new Vector3(0.0f, verticalSpeed, 0.0f) * Time.deltaTime;
+        transform.position += new Vector3(horizontalSpeed, 0.0f, 0.0f) * Time.deltaTime;
     }
 
     private void _CheckBounds()
     {
-        if (transform.position.y > verticalBoundary)
+        if (transform.position.x > horizontalBoundary)
         {
             bulletManager.ReturnBullet(gameObject);
         }
