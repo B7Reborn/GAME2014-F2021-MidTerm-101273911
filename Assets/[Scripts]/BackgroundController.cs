c9389e412be70131be9d004e34e5274e1470e04e
Shane <slholloway@mun.ca>
2021-10-23 21:59:41 -0230

    Changed project and background to landscape orientation

--- Assets/[Scripts]/BackgroundController.cs
+++ Assets/[Scripts]/BackgroundController.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
+﻿/*
+ * Full Name        : Shane Holloway
+ * Student ID       : 101273911
+ * Date Modified    : October 23, 2021
+ * File             : BackgroundController.cs
+ * Description      : Handles background scrolling, including speed and boundaries
+ * Revision History : V 1.0 - Initial change from vertical scrolling to horizontal scrolling
+ */
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
-    public float verticalSpeed;
-    public float verticalBoundary;
+    public float horizontalSpeed;
+    public float horizontalBoundary;
 
     // Update is called once per frame
     void Update()
@@ -16,18 +26,18 @@ public class BackgroundController : MonoBehaviour
 
     private void _Reset()
     {
-        transform.position = new Vector3(0.0f, verticalBoundary);
+        transform.position = new Vector3(horizontalBoundary, 0.0f);
     }
 
     private void _Move()
     {
-        transform.position -= new Vector3(0.0f, verticalSpeed) * Time.deltaTime;
+        transform.position -= new Vector3(horizontalSpeed, 0.0f) * Time.deltaTime;
     }
 
     private void _CheckBounds()
     {
         // if the background is lower than the bottom of the screen then reset
-        if (transform.position.y <= -verticalBoundary)
+        if (transform.position.x <= -horizontalBoundary)
         {
             _Reset();
         }
